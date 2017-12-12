c879c2e0d2bb61077981b5bd0a5fcaa69735d284
Unknown <matthewrmoyer@gmail.com>
2017-12-11 18:08:41 -0700

    add sqlalchemey

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/enviropi_db' 
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def index():
